a920ca9ecde96d3636f1187575a86b56d09ed417
quanfrontend <71162418+quanfrontend@users.noreply.github.com>
2020-10-07 10:18:53 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-    if (this.scrollY > 20) {
-      $(".navbar-container").addClass("sticky");
-    } else {
-      $(".navbar-container").removeClass("sticky");
-    }
-  });
-});
+
